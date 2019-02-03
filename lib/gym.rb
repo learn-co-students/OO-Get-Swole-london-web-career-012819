8ea107eb0b7c07bfe4707af90b537b1dc4cedf3c
github username <github email address>
2019-02-03 16:21:38 +0000

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,42 @@
 class Gym
-  attr_reader :name
+  attr_reader :name, :lifters
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.gym == self}
+  end
+
+  def lifters
+    self.memberships.map {|membership| membership.lifter}
+  end
+
+  def names_of_lifters
+    self.lifters.map {|lifter| lifter.name}
+  end
+
+  def lift_total
+    self.lifters.sum do |lifter|
+      lifter.lift_total
+    end
+  end
+
 end
+
+# - Get a list of all gyms [x]
+
+# - Get a list of all memberships at a specific gym [x]
+
+# - Get a list of all the lifters that have a membership to a specific gym [x]
+
+# - Get a list of the names of all lifters that have a membership to that gym [x]
+
+# - Get the combined lift total of every lifter has a membership to that gym
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,47 @@
 class Lifter
   attr_reader :name, :lift_total
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership if membership.lifter == self}
+  end
+
+  def gyms
+    self.memberships.map {|membership| membership.gym}
+    # self.memberships.map {|m| m.gym == self}
+  end
+
+  def sign_up(gym, membership_cost)
+    Membership.new(membership_cost, self, gym)
+  end
+
+  def self.average_lift
+    sum = self.all.sum { |lifter| lifter.lift_total }
+    sum / self.all.length
+  end
+
+  def total_membership_cost
+    self.memberships.sum {|membership| membership.cost}
   end
 end
+# - Get a list of all lifters [x]
+
+# - Get a list of all the memberships that a specific lifter has [x]
+
+# - Get a list of all the gyms that a specific lifter has memberships to [x]
+
+# - Get the average lift total of all lifters [x]
+
+# - Get the total cost of a specific lifter's gym memberships [x] 
+
+# - Given a gym and a membership cost, sign a specific lifter up for a new gym [x]
