d9a2dc1a02c2e4b5a17e54850a8fcfbba247169a
Tominiyi Ajayi <tominiyimunito@Tominiyis-MacBook-Air.local>
2019-02-04 20:07:23 +0000

    Done

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,36 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def list_specific_gym
+    Membership.all.select {|member| member.gym == self}
+  end
+
+  def specificlifter
+    list_specific_gym.map {|x| x.lifter}
+  end
+
+  def name_lifters
+    specificlifter.map {|n| n.name}
+  end
+
+  # def combined_total
+  #   total = list_specific_gym.select {|x| x.lifter.lift_total}
+  #   total.inject(:+)
+  # end
+
+  def combined
+    total = specificlifter.select {|x| lifter.lift_total}
+    total.inject(:+)
   end
 end
