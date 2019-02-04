0978c130018db15421e00f905f9f06313cbe0f0f
Kian Edalat <akedalat@gmail.com>
2019-02-04 19:37:05 +0000

    OO Gym completed by Kian

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,39 @@
 class Gym
-  attr_reader :name
+
+  attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+@@all
+  end
+
+  def memberships
+    Membership.all.select do |membership|
+    membership.gym == self
+    end
+  end
+
+  def lifters
+    self.memberships.map do |membership|
+      membership.lifter.name
+    end
+  end
+
+  def new_gym(gym, cost)
+    Membership.new(lifter, self, cost)
+  end
+
+def lift_total
+  lift_total = 0
+  self.memberships.map do |membership|
+    lift_total+= membership.lifter.lift_total
+  end
+  lift_total
+end
+
 end
