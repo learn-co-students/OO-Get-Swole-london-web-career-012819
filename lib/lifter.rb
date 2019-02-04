4d4d185b48427b04d7cb37321237f430d91dc813
Tominiyi Ajayi <tominiyimunito@Tominiyis-MacBook-Air.local>
2019-02-04 18:31:58 +0000

    Gym Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,43 @@
 class Lifter
-  attr_reader :name, :lift_total
+
+  attr_accessor :name
+  attr_reader :lift_total
+
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.lifter == self}
+  end
+
+  def gyms
+    memberships.map {|membership| membership.gym}
+  end
+
+  def self.averagelift
+    all_lift_totals = @@all.map {|lifter| lifter.lift_total }.inject(:+)/@@all.length
+    #OR
+    # total_lifts = all_lift_totals.inject(:+)
+    # total_lifts / @@all.length
+  end
+
+  def total_cost
+    total = memberships.map {|lifter| lifter.cost}
+    total.reduce(:+)
+  end
+
+  def sign_up(gym,cost)
+    Membership.new(self,gym,cost)
   end
 end
+
+# binding.pry
